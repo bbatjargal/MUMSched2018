09c9094c79d0cb10c4c8118a43a7237c162af83c
Batjargal Bayarsaikhan <b.batja@gmail.com>
2018-01-22 13:14:07 -0800

    starter project.
    using https://start.spring.io/

---
+++ src/main/java/mum/swe/mumsched/MumschedApplication.java
@@ -0,0 +1,12 @@
+package mum.swe.mumsched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumschedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumschedApplication.class, args);
+	}
+}
