71d5bc19d79b7198860cfddb02fd2f6affbe7ac0
Batjargal B <~b.batja@gmail.com>
2018-01-24 14:21:45 -0600

    modified layout and change thymeleaf mode to LEGAYHTML5

--- src/main/java/mum/swe/mumsched/model/User.java
+++ src/main/java/mum/swe/mumsched/model/User.java
@@ -2,17 +2,15 @@ package mum.swe.mumsched.model;
 
 import java.util.Set;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.JoinColumn;
 
 
 @Entity
