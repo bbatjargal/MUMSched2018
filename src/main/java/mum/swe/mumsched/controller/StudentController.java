7eb0ee87fdd31cbb64cbb6e58282f6aacc2baeb7
Mandaa109 <mandaa0109@gmail.com>
2018-02-05 16:48:45 -0600

    Student profile update
    Student profile update

--- src/main/java/mum/swe/mumsched/controller/StudentController.java
+++ src/main/java/mum/swe/mumsched/controller/StudentController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import mum.swe.mumsched.model.Faculty;
+import mum.swe.mumsched.model.Student;
 import mum.swe.mumsched.service.StudentService;
 
 /**
@@ -28,5 +31,14 @@ public class StudentController {
 	{
 		model.addAttribute("student", studentService.findByUsername(currentUser.getName()));
 		return "student/studentForm";
+	}
+	
+	@RequestMapping(value= {"/updateStudent"}, method = RequestMethod.POST)
+	public String updateStudentProfile(@ModelAttribute("student") Student student, Model model) {
+		Student studentDB = studentService.findOne(student.getId());
+		studentDB.getUser().setFirstName(student.getUser().getFirstName());
+		studentDB.getUser().setLastName(student.getUser().getLastName());
+		studentService.save(studentDB);
+		return "redirect:/studentprofile";
 	}
 }
