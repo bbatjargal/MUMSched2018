687a0b82c0e63ae65aab142f345897cda03eb946
Mandaa109 <mandaa0109@gmail.com>
2018-02-05 21:31:59 -0600

    Student profile update
    Medium ready

--- src/main/java/mum/swe/mumsched/controller/StudentController.java
+++ src/main/java/mum/swe/mumsched/controller/StudentController.java
@@ -6,39 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import mum.swe.mumsched.model.Faculty;
-import mum.swe.mumsched.model.Student;
+import mum.swe.mumsched.model.User;
 import mum.swe.mumsched.service.StudentService;
+import mum.swe.mumsched.service.UserService;
+import mum.swe.mumsched.validator.UserValidator;
 
 /**
  * @author Mandakh Nyamdavaa
  * @date Jan 28, 2018
  */
 
-
 @Controller
 public class StudentController {
 	
 	@Autowired
-	private StudentService studentService;
+	private UserService userService;
+	
+	 @Autowired
+	    private UserValidator userValidator;
 	
 	@RequestMapping(value="/studentprofile", method=RequestMethod.GET)
 	public String studentProfile(Model model, Pageable pageable, Principal currentUser)
 	{
-		model.addAttribute("student", studentService.findByUsername(currentUser.getName()));
+		model.addAttribute("user", userService.findByUsername(currentUser.getName()));
 		return "student/studentForm";
 	}
 	
-	@RequestMapping(value= {"/updateStudent"}, method = RequestMethod.POST)
-	public String updateStudentProfile(@ModelAttribute("student") Student student, Model model) {
-		Student studentDB = studentService.findOne(student.getId());
-		studentDB.getUser().setFirstName(student.getUser().getFirstName());
-		studentDB.getUser().setLastName(student.getUser().getLastName());
-		studentService.save(studentDB);
+	@RequestMapping(value = "/updateStudent", method = RequestMethod.POST)
+	public String adminProfile(@ModelAttribute("user") User user, 
+			BindingResult result, Principal currentUser, Model model)  {
+		User userRecord = userService.findByUsername(currentUser.getName());
+    	userValidator.validateAdmin(user, result);
+        System.out.println("*** hello ***");
+        userRecord.setFirstName(user.getFirstName());
+        userRecord.setLastName(user.getLastName());
+        userRecord.setPassword(user.getPassword());
+        
+		user = userService.save(userRecord);
+		model.addAttribute("user", userRecord);
 		return "redirect:/studentprofile";
 	}
 }
